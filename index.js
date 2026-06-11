83bea4e66d77f698e10fb7634220db9e96cbf007
Caua-Portugal <cauaportugal92@gmail.com>
2026-06-11 17:38:43 -0300

    terminei o projeto

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
 // Desafio classificador de nivel de heroi
 
 let nomeDoHeroi = "Zezinhho da Velocidade" 
-let xpDoHeroi = "7.356"
-let rankFerro = "1.000"
-let rankBronze = "1.001 e 2.000"
-let rankPrata = "2.001 e 5.000"
-let rankOuro = "5.001 e 7.000"
-let rankPlatina = "7.001 e 8.000"
-let rankAscendente = "8.001 e 9.000"
-let rankImortal = "9.001 e 10.000"
-let rankRadiante = "10.001"
-
-
-switch (xpDoHeroi) {
-    case "1.000": 
-        console.log ("O herói " + nomeDoHeroi + " está no nível: Ferro");
-    break;
-    case "1.001 e 2.000":
-        console.log ("O herói " + nomeDoHeroi + " está no nível: Bronze");
-        break;
-    case "2.001 e 5.000":
-        console.log ("O herói " + nomeDOHeroi + " está no nível: Prata");
-        break;
-    case "5.001 e 7.000":
-        console.log ("O herói " + nomeDoHeroi + " está no nível: Ouro");
-        break;
-    case "7.001 e 8.000":
-        console.log ("O herói " + nomeDoHeroi + " está no nível: Platina");
-        break;
-    case "8.001 e 9.000":
-        console.log ("O herói " + nomeDoHeroi + " está no nível: Ascendente");
-        break;
-    case "9.001 e 10.000":
-        console.log ("O herói " + nomeDoHeroi + " está no nível: Imortal");
-        break;
-    case "10.001":
-        console.log ("O herói " + nomeDoHeroi + " está no nível: Radiante");
-        break;} 
-
-    
+let xpDoHeroi = 7356
 
 
 
+if (xpDoHeroi <= 1000) {
+    console.log ("O herói de nome " + nomeDoHeroi + " está no nível de Ferro")
+}
+else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    console.log ("O herói de nome  " + nomeDoHeroi +  " está no nível de Bronze")
+}
+else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    console.log ("O herói de nome " + nomeDoHeroi + " está no nível de Prata")
+}
+else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+    console.log ("O herói de nome " + nomeDoHeroi + " está no nível de Ouro") 
+}
+else if (xpDoHeroi >=7001 && xpDoHeroi <= 8000) {
+    console.log ("O herói de nome " + nomeDoHeroi + " está no nível de Platina")
+}
+else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    console.log ("O herói de nome " + nomeDoHeroi + " está no nível de Ascendente")
+}
+else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    console.log ("O herói de nome " + nomeDoHeroi + " está no nível de Imortal")
+}
+else {
+    console.log ("O herói de nome " + nomeDoHeroi + " está no nível de Radiante")
+}
