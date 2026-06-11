0f84b633a5ae70fafab17a0fb93ea072493d4ecc
Caua-Portugal <cauaportugal92@gmail.com>
2026-06-11 15:29:11 -0300

    meu segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log ("OLA") 
+// Desafio classificador de nivel de heroi
+
+let nomeDoHeroi = "Zezinhho da Velocidade" 
+let xpDoHeroi = "7.356"
+let rankFerro = "1.001"
+let rankBronze = "1.001 e 2.000"
+let rankPrata = "2.001 e 5.000"
+let rankOuro = "5.001 e 7.000"
+let rankPlatina = "7.001 e 8.000"
+let rankAscendente = "8.001 e 9.000"
+let rankImortal = "9.001 e 10.000"
+let rankRadiante = "10.001"
+
+
+switch (xpDoHeroi) {
+    case "1.000": 
+        console.log ("O herói " + nomeDoHeroi + " está no nível: Ferro");
+    break;
+    case "1.001 e 2.000":
+        console.log ("O herói " + nomeDoHeroi + " está no nível: Bronze");
+        break;
+    case "2.001 e 5.000":
+        console.log ("O herói " + nomeDOHeroi + " está no nível: Prata");
+        break;
+    case "5.001 e 7.000":
+        console.log ("O herói " + nomeDoHeroi + " está no nível: Ouro");
+        break;
+    case "7.001 e 8.000":
+        console.log ("O herói " + nomeDoHeroi + " está no nível: Platina");
+        break;
+    case "8.001 e 9.000":
+        console.log ("O herói " + nomeDoHeroi + " está no nível: Ascendente");
+        break;
+    case "9.001 e 10.000":
+        console.log ("O herói " + nomeDoHeroi + " está no nível: Imortal");
+        break;
+    case "10.001":
+        console.log ("O herói " + nomeDoHeroi + " está no nível: Radiante");
+        break;} 
+
+    
+
+
+
